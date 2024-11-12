42eba4c25a3f065d142477d086861d4a20ccec4d
Faisal Rasbihan <faisalrasbihan@gmail.com>
2024-11-12 21:10:44 +0700

    fix pdf js error 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     return config;
+  },
+  output: 'standalone',
+  staticPageGenerationTimeout: 300,
+  experimental: {
+    optimizeCss: true,
   }
 };
 
